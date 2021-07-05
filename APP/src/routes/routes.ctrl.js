9ec465f7e1e65e3a418a3f60274cb079dfbea6b9
김지수 <kimji@MacBook-Pro.local>
2021-07-05 13:37:29 +0900

    /todolist 경로 생성

---
+++ APP/src/routes/routes.ctrl.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const todolist = {
+  read: (req, res) => {
+    res.render('index');
+  },
+
+  create: (req, res) => {
+    res.render('index');
+  },
+
+  update: (req, res) => {
+    res.render('index');
+  },
+
+  delete: (req, res) => {
+    res.render('index');
+  },
+};
+
+module.exports = {
+  todolist,
+};
