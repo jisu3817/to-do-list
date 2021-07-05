48eccdb47afeb99a1ee4cf60d9849c8acf06e4e5
김지수 <kimji@MacBook-Pro.local>
2021-07-05 14:10:35 +0900

    파일 경로 수정

---
+++ APP/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+const indexRouter = require('./src/routes');
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(`${__dirname}/src/public`));
+
+app.use('/', indexRouter);
+
+module.exports = app;
